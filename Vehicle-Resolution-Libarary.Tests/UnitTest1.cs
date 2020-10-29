4024b91754baed0243b8d01577add9062cf5e9f1
Stephen Odundo <steveremdi@gmail.com>
2020-10-29 14:12:37 +0300

    Added unit tests to check exceptions on null and correct reult with proper input

--- Vehicle-Resolution-Libarary.Tests/UnitTest1.cs
+++ Vehicle-Resolution-Libarary.Tests/UnitTest1.cs
@@ -1,13 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Vehicle_Resolution_Libarary.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private string GetResult(string input)
         {
+            var lib = new Start();
+            var result = lib.ResolveSingleWord(input);
+
+            return result;
         }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            string input = null;
+            var lib = new Start();
+            Assert.ThrowsException<ArgumentException>(()=> lib.ResolveSingleWord(input));
+        }
+
+        [TestMethod]
+        public void TestSampleWord()
+        {
+            var lib = new Start();
+            //var test = lib.ResolveSingleWord("SUNARU");
+            Assert.AreEqual(lib.ResolveSingleWord("SUNARU"), "SUBARU");
+        }
+
     }
 }
