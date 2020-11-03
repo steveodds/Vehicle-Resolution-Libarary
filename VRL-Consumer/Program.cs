b9d8a3c1479d174cbe6d800db25d8d3bb082780d
Stephen Odundo <steveremdi@gmail.com>
2020-11-03 14:13:14 +0300

    Added a test project to consume the class library

---
+++ VRL-Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Vehicle_Resolution_Libarary;
+
+namespace VRL_Consumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lib = new Start();
+            var result = lib.ResolveSingleWord("SUNARU", @"C:\Users\user\Documents\sample\Agilis LIVE Make & Models.csv");
+            Console.WriteLine(result);
+        }
+    }
+}
